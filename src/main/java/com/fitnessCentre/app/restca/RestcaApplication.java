42c6190460e572e033a97379df56a89b14564901
Bryan Nnadi <C22469126@mytudublin.ie>
2025-10-24 18:50:53 +0100

    Initial commit

---
+++ src/main/java/com/fitnessCentre/app/restca/RestcaApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessCentre.app.restca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessCentre/app/restca/RestcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessCentre.app.restca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
